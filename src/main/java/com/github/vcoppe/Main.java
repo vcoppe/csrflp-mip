ba0c8acdcb0a7f36d8adffacbbad82ed17161151
vcoppe <vianney.coppe@gmail.com>
2021-04-21 12:59:15 +0200

    first commit

---
+++ src/main/java/com/github/vcoppe/Main.java
@@ -0,0 +1,74 @@
+import gurobi.GRBException;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    private static int n = -1;
+    private static int[] l;
+    private static int[][] c;
+
+    private static void read(String path) {
+        try {
+            Scanner scan = new Scanner(new File(path));
+
+            int count = 0;
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                if (line.startsWith("c")) continue;
+
+                String[] tokens = line.split("[\\s,]+");
+                if (n != -1 && tokens.length != n) continue;
+
+                if (n == -1) {
+                    n = Integer.parseInt(tokens[0]);
+                    l = new int[n];
+                    c = new int[n][n];
+                } else if (count < n) {
+                    for (; count < n; count++) {
+                        l[count] = Integer.parseInt(tokens[count]);
+                    }
+                } else {
+                    int start = count;
+                    for (; count < start + n; count++) {
+                        c[count/n - 1][count % n] = Integer.parseInt(tokens[count % n]);
+                    }
+                }
+            }
+
+            scan.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to read input file");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) throws GRBException {
+        if (args.length < 1) {
+            System.out.println("Arguments needed :\n\tfilename\n\t[timeLimit]\n\t[threads]");
+            return;
+        }
+
+        read(args[0]);
+
+        int timeLimit = Integer.MAX_VALUE, threads = 0;
+        if (args.length >= 2) timeLimit = Integer.parseInt(args[1]);
+        if (args.length == 3) threads = Integer.parseInt(args[2]);
+
+        Model mip = new Model(n, l, c);
+
+        System.out.println("MIP model created");
+        System.out.println("Solving...");
+
+        mip.solve(timeLimit, threads);
+
+        System.out.println("runTime          : " + mip.runTime());
+        System.out.println("objValue         : " + mip.objVal());
+        System.out.println("gap              : " + mip.gap());
+
+        mip.dispose();
+    }
+
+}
---
+++ src/main/java/com/github/vcoppe/Model.java
@@ -0,0 +1,88 @@
+import gurobi.*;
+
+public class Model {
+
+    private GRBEnv env;
+    private GRBModel model;
+    private GRBVar[][][] z;
+
+    public Model(int n, int[] l, int[][] c) throws GRBException {
+        env = new GRBEnv("srflp.log");
+        model = new GRBModel(env);
+
+        z = new GRBVar[n][n][n];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (i != j) model.addVar(1, 1, 0.5 * l[i] * c[i][j], GRB.BINARY, "K_"+i+"_"+j);
+                for (int k=0; k<n; k++) {
+                    if (i == j || i == k || j == k) {
+                        z[i][j][k] = model.addVar(0, 0, 0, GRB.BINARY, "z_" + i + "_" + j + "_" + k);
+                    } else if (i < j) {
+                        z[i][j][k] = model.addVar(0, 1, c[i][j] * l[k], GRB.BINARY, "z_" + i + "_" + j + "_" + k);
+                    } else {
+                        z[i][j][k] = model.addVar(0, 1, 0, GRB.BINARY, "z_" + i + "_" + j + "_" + k);
+                    }
+                }
+            }
+        }
+
+        GRBLinExpr expr;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                for (int k=0; k<n; k++) if (k != i && k != j) {
+                    model.addConstr(z[i][j][k], GRB.EQUAL, z[j][i][k], "equal_"+i+"_"+j+"_"+k);
+
+                    expr = new GRBLinExpr();
+                    expr.addConstant(1);
+                    expr.addTerm(-1, z[i][k][j]);
+                    expr.addTerm(-1, z[j][k][i]);
+                    model.addConstr(z[i][j][k], GRB.EQUAL, expr, "transform_equal_"+i+"_"+j+"_"+k);
+
+                    expr = new GRBLinExpr();
+                    expr.addTerm(1, z[i][j][k]);
+                    expr.addTerm(1, z[i][k][j]);
+                    expr.addTerm(1, z[j][k][i]);
+                    model.addConstr(expr, GRB.EQUAL, 1, "exclusive_"+i+"_"+j+"_"+k);
+
+                    for (int d=0; d<n; d++) if (d != i && d != j && d != k) {
+                        expr = new GRBLinExpr();
+                        expr.addTerm(1, z[i][j][d]);
+                        expr.addTerm(1, z[j][k][d]);
+                        expr.addTerm(-1, z[i][k][d]);
+                        model.addConstr(expr, GRB.GREATER_EQUAL, 0, "forced_"+i+"_"+j+"_"+k+"_"+d);
+
+                        expr = new GRBLinExpr();
+                        expr.addTerm(1, z[i][j][d]);
+                        expr.addTerm(1, z[j][k][d]);
+                        expr.addTerm(1, z[i][k][d]);
+                        model.addConstr(expr, GRB.LESS_EQUAL, 2, "exclusive_"+i+"_"+j+"_"+k+"_"+d);
+                    }
+                }
+            }
+        }
+    }
+
+    public void solve(double timeLimit, int threads) throws GRBException {
+        model.set(GRB.DoubleParam.TimeLimit, timeLimit);
+        if (threads > 0) model.set(GRB.IntParam.Threads, threads);
+        model.optimize();
+    }
+
+    public double gap() throws GRBException {
+        return model.get(GRB.DoubleAttr.MIPGap);
+    }
+
+    public double runTime() throws GRBException {
+        return model.get(GRB.DoubleAttr.Runtime);
+    }
+
+    public double objVal() throws GRBException {
+        return model.get(GRB.DoubleAttr.ObjVal);
+    }
+
+    public void dispose() throws GRBException {
+        model.dispose();
+        env.dispose();
+    }
+}
